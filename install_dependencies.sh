7ef6f7fdfa50dc5ba1efe5dce3b5b285475779ce
SpyterDev <spyterdev@gmail.com>
2024-05-31 23:39:56 -0400

    Removed Mingw-w64
    - You do not need Mingw-w64 or audiofile to compile the game (at least when I tested it)

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,4 @@
 
 # Installs all the need dependencies from the Homebrew Package Manager to compile sm64ex (Super Mario 64 EX)
 
-brew install gcc make sdl2 glew glfw pkg-config mingw-w64 dylibbundler
+brew install make sdl2 glew glfw pkg-config dylibbundler
